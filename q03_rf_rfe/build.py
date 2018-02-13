35b5b190c2fd7177f1708f8338c69d06a9e0028b
djocz <jocelynd789@gmail.com>
2018-02-13 04:34:28 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -8,5 +8,10 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
-
-
+def rf_rfe(df):
+    X,y = df.iloc[:,:-1],df.iloc[:,-1]
+    model = RandomForestClassifier()
+    features_no = X.columns
+    rfe = RFE(model,len(X.columns)/2)
+    rfe.fit(X,y)
+    return X.columns.values[rfe.get_support()].tolist()
